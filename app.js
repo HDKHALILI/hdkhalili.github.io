ae27f68f4a9e29a714d9f85a511207de075d6eca
= <hd_khalili@yahoo.com.au>
2018-03-26 12:42:54 +1100

    Added site map verification

--- app.js
+++ app.js
@@ -2,25 +2,33 @@
 const front_end_btn = document.querySelector(".front-end-btn");
 const back_end_btn = document.querySelector(".back-end-btn");
 const design_btn = document.querySelector(".design-btn");
-
+const html = document.querySelector("html");
 // divs to be effected
 const front_end = document.querySelector(".front-end");
 const back_end = document.querySelector(".back-end");
 const design = document.querySelector(".design");
 
+
+function makeVisible() {
+  front_end.classList.remove("hidden");
+}
+
 front_end_btn.addEventListener('click', function() {
-  front_end.style.display = 'grid';
-  back_end.style.display = 'none'
-  design.style.display = 'none'
+  front_end.classList.remove('hidden');
+  back_end.classList.add('hidden');
+  // back_end.classList.add('make-visible');
+  design.classList.add('hidden');
+  
   front_end_btn.classList.add("selected");
   back_end_btn.classList.remove("selected");
   design_btn.classList.remove("selected");
 });
 
 back_end_btn.addEventListener('click', function () {
-  back_end.style.display = 'grid'
-  front_end.style.display = 'none';
-  design.style.display = 'none'
+  back_end.classList.remove('hidden');
+  front_end.classList.remove('make-visible');
+  front_end.classList.add('hidden');
+  design.classList.add('hidden');  
 
   back_end_btn.classList.add("selected");
   front_end_btn.classList.remove("selected");
@@ -28,9 +36,11 @@ back_end_btn.addEventListener('click', function () {
 });
 
 design_btn.addEventListener('click', function () {
-  design.style.display = 'grid'
-  front_end.style.display = 'none';
-  back_end.style.display = 'none'
+  design.classList.remove('hidden');   
+  back_end.classList.add('hidden');
+  front_end.classList.add('hidden');
+  front_end.classList.remove('make-visible');
+
 
   design_btn.classList.add("selected");
   front_end_btn.classList.remove("selected");
